6687143178f036b63ab3987a48c9f0126d8d242e
Lanceconan <conan_022@hotmail.com>
2018-03-20 13:22:21 -0300

    Metodos JAVA para sumar dos numeros

---
+++ holaMundoJAVA/Metodos.java
@@ -0,0 +1,15 @@
+public class Metodos {
+  
+  public static void main(String[] args) {
+    System.out.println("la suma es: " + getSuma(3, 2));
+  }
+  /**
+    * Metodo para sumar dos numeros simples
+	*
+  */
+  private int getSuma(int nu1, int nu2){
+	return nu1 + nu2;  
+	  
+  }
+	  
+}
